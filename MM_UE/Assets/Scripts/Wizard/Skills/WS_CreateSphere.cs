02f68c6b45c9e50b788ddf62958187040ac6d26a
DESKTOP-B4LNKON\stefan <stefan.steinheber@gmail.com>
2023-04-24 16:32:36 +0200

    #12: Added library for type references and two dummy skills for the wizard

---
+++ MM_UE/Assets/Scripts/Wizard/Skills/WS_CreateSphere.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WS_CreateSphere : WizardSkill
+{
+    public GameObject boxPrefab;
+
+    public override void OnExecute(GameObject wizard)
+    {
+        GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        cube.transform.position = wizard.transform.position;
+    }
+}
---
+++ MM_UE/Assets/Scripts/Wizard/WizardSkillBehaviour.cs
@@ -0,0 +1,35 @@
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+using TypeReferences;
+
+public class WizardSkillBehaviour : MonoBehaviour
+{
+    string[] skills = Assembly.GetAssembly(typeof(WizardSkill)).GetTypes()
+        .Where(myType => myType.IsClass && !myType.IsAbstract && !myType.IsGenericType && myType.IsSubclassOf(typeof(WizardSkill)))
+        .Select(type => type.ToString()).ToArray();
+    [Inherits(typeof(WizardSkill))]
+    public TypeReference[] availableWizardSkills;
+
+    int selectedSkill;
+    WizardSkill[] availableWizardSkillInstances;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Transforming all selected Skills at runtime to an array of instances improves performance and memory usage
+        availableWizardSkillInstances =
+            availableWizardSkills.Select(skillType => Assembly.GetAssembly(typeof(WizardSkillBehaviour)).CreateInstance(skillType.Type.Name) as WizardSkill).ToArray();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("WizardInteract"))
+        {
+            availableWizardSkillInstances[selectedSkill].OnExecute(gameObject);
+        }
+
+        selectedSkill = (selectedSkill + (int)Input.mouseScrollDelta.y) % availableWizardSkillInstances.Length;
+    }
+}

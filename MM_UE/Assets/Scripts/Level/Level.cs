f7c698c71e3817f034b083a931d59c0dd262a4ad
DESKTOP-B4LNKON\stefan <stefan.steinheber@gmail.com>
2023-06-01 23:01:30 +0200

    #28: added subdivisions for the wall mesh

--- MM_UE/Assets/Scripts/Level/Level.cs
+++ MM_UE/Assets/Scripts/Level/Level.cs
@@ -118,68 +118,229 @@ public abstract class Level : MonoBehaviour
 
     private void createWallGameObjects()
     {
-
+        int wallExtent = (int) (Math.Max(levelWidth, Math.Max(levelHeight, levelDepth)) * 1.7);
         // front wall
-        createPlaneForCorners(new Vector3(0, 0, 0), new Vector3(levelWidth, 0, 0), new Vector3(levelWidth, levelHeight, 0), new Vector3(0, levelHeight, 0));
+        createPlaneForCorners(
+            new Vector3(0, 0, 0),
+            new Vector3(levelWidth, 0, 0),
+            new Vector3(levelWidth, levelHeight, 0),
+            new Vector3(0, levelHeight, 0)
+        );
         // hind wall
-        createPlaneForCorners(new Vector3(0, 0, levelDepth), new Vector3(0, levelHeight, levelDepth), new Vector3(levelWidth, levelHeight, levelDepth), new Vector3(levelWidth, 0, levelDepth));
+        createPlaneForCorners(
+            new Vector3(0, 0, levelDepth),
+            new Vector3(0, levelHeight, levelDepth),
+            new Vector3(levelWidth, levelHeight, levelDepth),
+            new Vector3(levelWidth, 0, levelDepth)
+        );
         //left wall
-        createPlaneForCorners(new Vector3(levelWidth, 0, 0), new Vector3(levelWidth, 0, levelDepth), new Vector3(levelWidth, levelHeight, levelDepth), new Vector3(levelWidth, levelHeight, 0));
+        createPlaneForCorners(
+            new Vector3(levelWidth, 0, 0),
+            new Vector3(levelWidth, 0, levelDepth),
+            new Vector3(levelWidth, levelHeight, levelDepth),
+            new Vector3(levelWidth, levelHeight, 0)
+        );
         // right wall
-        createPlaneForCorners(new Vector3(0, levelHeight, 0), new Vector3(0, levelHeight, levelDepth), new Vector3(0, 0, levelDepth), new Vector3(0, 0, 0));
+        createPlaneForCorners(
+            new Vector3(0, levelHeight, 0),
+            new Vector3(0, levelHeight, levelDepth),
+            new Vector3(0, 0, levelDepth),
+            new Vector3(0, 0, 0)
+        );
         // Ceiling
-        createPlaneForCorners(new Vector3(0, levelHeight, 0), new Vector3(levelWidth, levelHeight, 0), new Vector3(levelWidth, levelHeight, levelDepth), new Vector3(0, levelHeight, levelDepth));
+        createPlaneForCorners(
+            new Vector3(0, levelHeight, 0),
+            new Vector3(levelWidth, levelHeight, 0),
+            new Vector3(levelWidth, levelHeight, levelDepth),
+            new Vector3(0, levelHeight, levelDepth)
+        );
 
-        // bottom wall
-        createPlaneForCorners(new Vector3(0, 0, 0), new Vector3(levelWidth, 0, 0), new Vector3(levelWidth, -100, 0), new Vector3(0, -100, 0));
-        // left Wall
-        createPlaneForCorners(new Vector3(-100, -100, 0), new Vector3(-100, 100, 0), new Vector3(0, 100, 0), new Vector3(0, -100, 0));
-        // right wall
-        createPlaneForCorners(new Vector3(levelWidth, -100, 0), new Vector3(levelWidth, 100, 0), new Vector3(100, 100, 0), new Vector3(100, -100, 0));
-        // top wall
-        createPlaneForCorners(new Vector3(0, 100, 0), new Vector3(levelWidth, 100, 0), new Vector3(levelWidth, levelHeight, 0), new Vector3(0, levelHeight, 0));
-        
-        // TODO: add bound walls for other perspectives
+        // bound walls for front side
+        createPlaneForCorners(
+            new Vector3(0, 0, 0), 
+            new Vector3(levelWidth, 0, 0), 
+            new Vector3(levelWidth, -wallExtent, 0),
+            new Vector3(0, -wallExtent, 0)
+        );
+        createPlaneForCorners(
+            new Vector3(-wallExtent, -wallExtent, 0),
+            new Vector3(-wallExtent, wallExtent, 0),
+            new Vector3(0, wallExtent, 0),
+            new Vector3(0, -wallExtent, 0)
+        );
+        createPlaneForCorners(
+            new Vector3(levelWidth, -wallExtent, 0),
+            new Vector3(levelWidth, wallExtent, 0),
+            new Vector3(wallExtent, wallExtent, 0),
+            new Vector3(wallExtent, -wallExtent, 0)
+        );
+        createPlaneForCorners(
+            new Vector3(0, wallExtent, 0),
+            new Vector3(levelWidth, wallExtent, 0),
+            new Vector3(levelWidth, levelHeight, 0),
+            new Vector3(0, levelHeight, 0)
+        );
+
+        // bound walls for left side
+        createPlaneForCorners(
+            new Vector3(0, levelHeight, 0),
+            new Vector3(0, levelHeight, levelDepth),
+            new Vector3(0, wallExtent, levelDepth),
+            new Vector3(0, wallExtent, 0)
+        );
+        createPlaneForCorners(
+            new Vector3(0, -wallExtent, 0), 
+            new Vector3(0, -wallExtent, levelDepth), 
+            new Vector3(0, 0, levelDepth), 
+            new Vector3(0, 0, 0)
+        );
+        createPlaneForCorners(
+            new Vector3(0, -wallExtent, levelDepth), 
+            new Vector3(0, -wallExtent, wallExtent), 
+            new Vector3(0, wallExtent, wallExtent), 
+            new Vector3(0, wallExtent, levelDepth)
+        );
+        createPlaneForCorners(
+            new Vector3(0, wallExtent, 0),
+            new Vector3(0, wallExtent, -wallExtent),
+            new Vector3(0, -wallExtent, -wallExtent),
+            new Vector3(0, -wallExtent, 0)
+        );
+
+        //bound walls for back side
+        createPlaneForCorners(
+            new Vector3(0, -wallExtent, levelDepth),
+            new Vector3(levelWidth, -wallExtent, levelDepth),
+            new Vector3(levelWidth, 0, levelDepth),
+            new Vector3(0, 0, levelDepth)
+        );
+        createPlaneForCorners(
+            new Vector3(0, -wallExtent, levelDepth),
+            new Vector3(0, wallExtent, levelDepth),
+            new Vector3(-wallExtent, wallExtent, levelDepth),
+            new Vector3(-wallExtent, -wallExtent, levelDepth)
+        );
+        createPlaneForCorners(
+            new Vector3(wallExtent, -wallExtent, levelDepth),
+            new Vector3(wallExtent, wallExtent, levelDepth),
+            new Vector3(levelWidth, wallExtent, levelDepth),
+            new Vector3(levelWidth, -wallExtent, levelDepth)
+        );
+        createPlaneForCorners(
+            new Vector3(0, levelHeight, levelDepth),
+            new Vector3(levelWidth, levelHeight, levelDepth),
+            new Vector3(levelWidth, wallExtent, levelDepth),
+            new Vector3(0, wallExtent, levelDepth)
+        );
+
+
+        // bound walls for right side
+        createPlaneForCorners(
+            new Vector3(levelWidth, wallExtent, 0),
+            new Vector3(levelWidth, wallExtent, levelDepth),
+            new Vector3(levelWidth, levelHeight, levelDepth),
+            new Vector3(levelWidth, levelHeight, 0)
+        );
+        createPlaneForCorners(
+            new Vector3(levelWidth, 0, 0),
+            new Vector3(levelWidth, 0, levelDepth),
+            new Vector3(levelWidth, -wallExtent, levelDepth),
+            new Vector3(levelWidth, -wallExtent, 0)
+        );
+        createPlaneForCorners(
+            new Vector3(levelWidth, wallExtent, levelDepth),
+            new Vector3(levelWidth, wallExtent, wallExtent),
+            new Vector3(levelWidth, -wallExtent, wallExtent),
+            new Vector3(levelWidth, -wallExtent, levelDepth)
+        );
+        createPlaneForCorners(
+            new Vector3(levelWidth, -wallExtent, 0),
+            new Vector3(levelWidth, -wallExtent, -wallExtent),
+            new Vector3(levelWidth, wallExtent, -wallExtent),
+            new Vector3(levelWidth, wallExtent, 0)
+        );
     }
 
     private void createPlaneForCorners(Vector3 corner1, Vector3 corner2, Vector3 corner3, Vector3 corner4)
     {
-        GameObject plane = GameObject.CreatePrimitive(PrimitiveType.Plane);
-        Mesh mesh = new Mesh();
         Vector3 half = Vector3.one / 2;
-        mesh.vertices = new Vector3[]
+        corner1 = corner1 - half;
+        corner2 = corner2 - half;
+        corner3 = corner3 - half;
+        corner4 = corner4 - half;
+
+        float unitLength = 1.0f; // Längeneinheit
+
+        // Berechne die Anzahl der Längeneinheiten in beiden Dimensionen
+        int widthSegments = Mathf.FloorToInt(Vector3.Distance(corner1, corner2) / unitLength);
+        int lengthSegments = Mathf.FloorToInt(Vector3.Distance(corner1, corner4) / unitLength);
+
+        // Erzeuge das Gesamt-Mesh
+        Mesh mesh = new Mesh();
+
+        // Berechne die Anzahl der Vertices, Dreiecke und UVs
+        int numVertices = (widthSegments + 1) * (lengthSegments + 1);
+        int numTriangles = widthSegments * lengthSegments * 2;
+        int numUVs = numVertices;
+
+        // Arrays für Vertices, Normals, Triangles und UVs erstellen
+        Vector3[] vertices = new Vector3[numVertices];
+        Vector3[] normals = new Vector3[numVertices];
+        int[] triangles = new int[numTriangles * 3];
+        Vector2[] uvs = new Vector2[numUVs];
+
+        // Berechne den Halb-Offset
+        Vector3 halfOffset = new Vector3(unitLength, 0.0f, unitLength) / 2;
+
+        // Erzeuge die Vertices, Normals und UVs
+        for (int i = 0; i <= lengthSegments; i++)
         {
-            corner1 - half,
-            corner2 - half,
-            corner3 - half,
-            corner4 - half
-        };
+            for (int j = 0; j <= widthSegments; j++)
+            {
+                int index = i * (widthSegments + 1) + j;
 
-        mesh.triangles = new int[]
+                // Berechne die Position des Vertices
+                Vector3 position = corner1 + (j * unitLength / widthSegments) * (corner2 - corner1) + (i * unitLength / lengthSegments) * (corner4 - corner1);
+
+                // Setze den Vertex, Normal und UV
+                vertices[index] = position;
+                normals[index] = Vector3.Cross(corner4 - corner1, corner2 - corner1).normalized;
+                uvs[index] = new Vector2((float)j / widthSegments, (float)i / lengthSegments);
+            }
+        }
+
+        // Erzeuge die Dreiecke
+        int triangleIndex = 0;
+        for (int i = 0; i < lengthSegments; i++)
         {
-            0,1,2,2,3,0
-        };
+            for (int j = 0; j < widthSegments; j++)
+            {
+                int vertexIndex = i * (widthSegments + 1) + j;
 
-        Plane geoPlane = new Plane();
-        geoPlane.Set3Points(corner1, corner2, corner3);
-        Vector3 normal = geoPlane.normal;
+                // Erzeuge das erste Dreieck
+                triangles[triangleIndex] = vertexIndex;
+                triangles[triangleIndex + 1] = vertexIndex + 1;
+                triangles[triangleIndex + 2] = vertexIndex + widthSegments + 1;
 
-        mesh.normals = new Vector3[] {
-            normal,
-            normal,
-            normal,
-            normal
-        };
+                // Erzeuge das zweite Dreieck
+                triangles[triangleIndex + 3] = vertexIndex + widthSegments + 1;
+                triangles[triangleIndex + 4] = vertexIndex + 1;
+                triangles[triangleIndex + 5] = vertexIndex + widthSegments + 2;
 
-        plane.GetComponent<MeshFilter>().mesh = mesh;
-        plane.GetComponent<MeshCollider>().sharedMesh = mesh;
+                triangleIndex += 6;
+            }
+        }
 
-        plane.GetComponent<MeshRenderer>().material = wallMaterial;
+        // Setze das Mesh
+        mesh.vertices = vertices;
+        mesh.normals = normals;
+        mesh.triangles = triangles;
+        mesh.uv = uvs;
 
-        /*
-        plane.transform.position = (corner1 + corner2 + corner3 + corner4) / 4;
-        Bounds bounds = plane.GetComponent<MeshFilter>().mesh.bounds;
-        bounds.extents = new Vector3(20, 0, 50);
-        */
+        // Erzeuge das GameObject
+        GameObject plane = new GameObject("Plane");
+        plane.AddComponent<MeshFilter>().mesh = mesh;
+        plane.AddComponent<MeshRenderer>().material = wallMaterial;
+        plane.AddComponent<MeshCollider>().sharedMesh = mesh;
     }
 }

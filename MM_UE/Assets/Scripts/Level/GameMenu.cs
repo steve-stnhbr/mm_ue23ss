6b03f935504a9d930afa3440349ff4fc975fb108
AdrianKzbr <59136635+AdrianKzbr@users.noreply.github.com>
2023-06-05 22:19:20 +0200

    added different UI Elements with corresponding Scripts

---
+++ MM_UE/Assets/Scripts/Level/GameMenu.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class GameMenu : MonoBehaviour
+{
+    [SerializeField] GameObject GameMenuUI;
+    [SerializeField] GameObject LoadingScreenUI;
+    [SerializeField] GameObject LevelSelectUI;
+    
+
+
+    private void OnEnable()
+    {
+        
+    }
+
+    public void loadFirstLevel()
+    {
+        
+    }
+}
--- MM_UE/Assets/Scripts/UI/LevelSelectUI.cs
+++ MM_UE/Assets/Scripts/UI/LevelSelectUI.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LevelLoader : MonoBehaviour
+public class LevelSelectUI : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()

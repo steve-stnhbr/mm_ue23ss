5de16c0182cf0e2a82ccb9bc7bbe54aa378edda7
AdrianKzbr <59136635+AdrianKzbr@users.noreply.github.com>
2023-06-06 18:02:58 +0200

    Added HUD, Level Reset, Volume Control

--- MM_UE/Assets/Scripts/UI/MainMenuUI.cs
+++ MM_UE/Assets/Scripts/UI/MainMenuUI.cs
@@ -7,6 +7,9 @@ public class MainMenuUI : MonoBehaviour
 {
     [SerializeField] GameMenu gameMenu;
 
+    SliderInt volumeSlider;
+    Label volumeLabel;
+
     private void OnEnable()
     {
 
@@ -15,10 +18,23 @@ public class MainMenuUI : MonoBehaviour
         Button buttonNewGame = root.Q<Button>("ButtonNewGame");
         Button buttonLevelSelector = root.Q<Button>("ButtonLevelSelector");
         Button buttonQuit = root.Q<Button>("ButtonQuit");
+        volumeSlider = root.Q<SliderInt>("VolumeSlider");
+        volumeLabel = root.Q<Label>("VolumeLabel");
 
         buttonNewGame.clicked += () => gameMenu.loadLevel(1);
         buttonLevelSelector.clicked += () => gameMenu.openLevelSelector();
         buttonQuit.clicked += () => Application.Quit();
         buttonQuit.clicked += () => Debug.Log("Quit does not work in the editor");
+
+        volumeSlider.value = (int)(AudioListener.volume * 100f);
+        volumeLabel.text = (int)(AudioListener.volume * 100f) + "%";
+
+    }
+
+    private void Update()
+    {
+        Debug.Log(AudioListener.volume);
+        AudioListener.volume = volumeSlider.value/100f;
+        volumeLabel.text = volumeSlider.value + "%";
     }
 }

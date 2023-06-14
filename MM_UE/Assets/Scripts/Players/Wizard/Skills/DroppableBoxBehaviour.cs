a23df5ae43bcfca36eafe1946c6763f3431af6ae
DESKTOP-B4LNKON\stefan <stefan.steinheber@gmail.com>
2023-06-14 22:57:54 +0200

    added sound for collision of droppablebox

---
+++ MM_UE/Assets/Scripts/Players/Wizard/Skills/DroppableBoxBehaviour.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class DroppableBoxBehaviour : MonoBehaviour
+{
+    [Tooltip("This AduioClip is played when the box hits the floor")]
+    public AudioClip dropClip;
+
+    AudioSource audioSource;
+    bool dropped;
+    short framesAliveBeforeDrop;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        dropped = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!dropped)
+        {
+            framesAliveBeforeDrop++;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!dropped && framesAliveBeforeDrop > 3 && collision.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        {
+            dropped = true;
+            this.playDropSound();
+        }
+    }
+
+    private void playDropSound()
+    {
+        audioSource.PlayOneShot(dropClip);
+    }
+}

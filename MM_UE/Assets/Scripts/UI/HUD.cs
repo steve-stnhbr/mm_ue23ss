203ed280d65551b2f6755cc75690328eb75e44f8
AdrianKzbr <59136635+AdrianKzbr@users.noreply.github.com>
2023-06-14 15:46:55 +0200

    added positionMarker for wizard

--- MM_UE/Assets/Scripts/UI/HUD.cs
+++ MM_UE/Assets/Scripts/UI/HUD.cs
@@ -6,6 +6,8 @@ using UnityEngine.UIElements;
 public class HUD : MonoBehaviour
 {
     [SerializeField] GameMenu gameMenu;
+    [TextArea(15, 20)]
+    [SerializeField] string levelTip;
 
     private void OnEnable()
     {
@@ -14,8 +16,13 @@ public class HUD : MonoBehaviour
 
         Button buttonReset = root.Q<Button>("ButtonReset");
         Button buttonMenu = root.Q<Button>("ButtonMenu");
+        Label labelInfo = root.Q<Label>("LabelInfo");
 
         buttonReset.clicked += () => gameMenu.restartLevel();
         buttonMenu.clicked += () => gameMenu.togglePauseMenu();
+        if(levelTip != null || !levelTip.Equals(""))
+        {
+            labelInfo.text = levelTip;
+        }
     }
 }
